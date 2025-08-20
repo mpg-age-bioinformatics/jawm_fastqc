9d001a81ffbd508a0f67954ce88222a6a0cb8e12
jorgeboucas <jboucas@age.mpg.de>
2025-08-20 22:43:29 +0200

    cleaning

---
+++ test/test.py
@@ -0,0 +1,50 @@
+
+# usage: python3 fastqc.py
+
+import sys
+import os
+from pathlib import Path
+
+# add parent folder to sys.path to be able to import module
+sys.path.append("..")
+from fastqc import *
+
+if ( not jawm.utils.docker_available() ) and ( not jawm.utils.apptainer_available() ) :
+
+    print("Neither Docker nor Apptainer could be found, stopping test...")
+    sys.exit(1)
+
+# define script input and output
+input_file=Path("test.fastq.gz").resolve()
+output_folder=Path("../test-output").resolve()
+
+# make sure output folder exists
+if not os.path.isdir(output_folder):
+    os.makedirs(output_folder)
+
+# add variables to script
+fastqc.var={
+    "output":output_folder,
+    "f":input_file,
+    "extra_args": "",
+} 
+
+# set environment docker/apptainer
+if jawm.utils.apptainer_available(v=True) :
+    fastqc.param_file="../yaml/apptainer.params.yaml" 
+    fastqc.environment_apptainer={ '-B': [input_file, output_folder] }
+    
+if jawm.utils.docker_available(v=True) :
+    fastqc.param_file="../yaml/docker.params.yaml" 
+    fastqc.environment_apptainer={ '-v': [input_file, output_folder] }
+
+# execute process
+fastqc.execute()
+
+# wait for all processes to complete
+jawm.Process.wait()
+
+# print the output
+print(fastqc.get_output())
+
+jawm.utils.write_hash_file(output_folder, "test.hash")
