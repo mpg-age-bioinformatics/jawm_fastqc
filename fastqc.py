c92ee781dfdfff56fb87ee83c7ef7db233dcb44b
jorgeboucas <jboucas@age.mpg.de>
2025-08-20 16:24:42 +0200

    dev

--- fastqc.py
+++ fastqc.py
@@ -15,18 +15,44 @@
 
 import jawm
 
+# {{{
 fastqc=jawm.Process( 
     name="fastqc",
     script="""#!/bin/bash
 fastqc {{extra_args}} -t {{ncores}} -o {{output}} {{f}}
 """,
+
+    # example arguments :
+    
     # script_variables={
     #     "extra_args": "",
     #     "ncores":"<n.cores>", 
     #     "output":"<output_folder>",
     #     "f": "<input_file>",
-    # }
+    # },
+    
+    # manager="slurm",
+    # manager_slurm={
+    #     "-p":"cluster,dedicated", 
+    #     "--mem":"20GB", 
+    #     "-t":"1:00:00", 
+    #     "-c":"8" 
+    # },
+    
+    # container="docker://mpgagebioinformatics/fastqc:0.11.9",
+    # environmnent="apptainer",
+    # environment_apptainer={ '-B': [input_file, output_folder] }
+    
+    # container="mpgagebioinformatics/fastqc:0.11.9",
+    # environmnent="docker",
+    # environment_docker={ '-v': [input_file, output_folder] },
+
+
+    # param_file="yaml/apptainer.params.yaml" ,
+    # param_file=[ "yaml/apptainer.params.yaml" , "yaml/slurm.params.yaml" ],
+  
 )
+# }}}
 
 
 if __name__ == "__main__": 
@@ -66,7 +92,6 @@ if __name__ == "__main__":
             fastqc.param_file="yaml/apptainer.params.yaml" 
             fastqc.environment_apptainer={ '-B': [input_file, output_folder] }
             
-
         if jawm.utils.docker_available(v=True) :
             fastqc.param_file="yaml/docker.params.yaml" 
             fastqc.environment_apptainer={ '-v': [input_file, output_folder] }
