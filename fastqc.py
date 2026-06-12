d5f4823de2d218c13aa7d7e5fb60e6b5b7fe6b2d
Jorge Boucas <jboucas@age.mpg.de>
2026-06-12 17:53:07 +0000

    cleaned and tutorial mirror

--- fastqc.py
+++ fastqc.py
@@ -1,125 +1,113 @@
 import jawm
+import sys
 import os
-from pathlib import Path
 
+# define our fastqc process
+fastqc=jawm.Process(
 
-# {{{
-fastqc=jawm.Process( 
-    name="fastqc",
-    when=lambda p: not os.path.isfile( os.path.join( p.var["fastqc_output"], os.path.basename( str(p.var["f"]).lstrip().split(" ")[0].split( ".fastq.gz"  )[0].split( ".fq.gz"  )[0] )+"_fastqc.html" )  ) ,
-    script="""#!/bin/bash
+  # Logical statement to be validated prior to execution
+  when=lambda p: not os.path.isfile(
+                        os.path.join(
+                          p.var["fastqc_output"],
+                          os.path.basename( str(p.var["f"]).lstrip().split(" ")[0].split( ".fastq.gz"  )[0].split( ".fq.gz"  )[0] )+"_fastqc.html"
+                        )
+                      ),
+
+  # Process name
+  name="fastqc",
+
+  # Script to be run when process is executed
+  script="""#!/bin/bash
 fastqc {{extra_args}} -t {{cpus}} -o {{fastqc_output}} {{f}}
 """,
-    
-    # example arguments :
-    var={"extra_args": ""}, 
-    # var={
-    #     "extra_args": "",
-    #     "cpus":"<n.cores>", 
-    #     "mk.output":"<output_folder>",
-    #     "map.f": "<input_file>",
-    # },
-    
-    # manager="slurm",
-    manager_slurm={
-        "--mem":"20GB", 
-        "-t":"1:00:00", 
-        "-c":"4" 
-    },
-    
-    # container="docker://mpgagebioinformatics/fastqc:0.11.9",
-    # environmnent="apptainer",
-    # environment_apptainer={ '-B': [input_file, output_folder] }
-    
-    container="mpgagebioinformatics/fastqc:0.11.9",
-    # environmnent="docker",
-    # environment_docker={ '-v': [input_file, output_folder] },
 
+  # Description of the variables
+  desc={
+    "extra_args":"Any fastqc argument with its respective value.",
+    "cpus":"Number of cpus to be used.",
+    "fastqc_output":"Output dir.",
+    "f":"Input fastq file."
+  },
 
-    # param_file="yaml/apptainer.params.yaml" ,
-    # param_file=[ "yaml/apptainer.params.yaml" , "yaml/slurm.params.yaml" ],
-  
+  # Default variable values
+  var={
+    "extra_args":"",
+    "cpus":1
+  },
+
+  # image and respective environment
+  container="mpgagebioinformatics/fastqc:0.11.9", # defaults to docker://mpgagebioinformatics/fastqc:0.11.9
+  environment="docker", # docker or apptainer
+
+  # slurm default arguments
+  manager_slurm={
+    "--mem":"20GB",
+    "-t":"1:00:00",
+    "-c":"4"
+  }
+
 )
 
-def report_files(fastqc_output) :
-    report_paths={}
-    dic={ 
-        fastqc_output : { 
-            "fastqc":"*_fastqc.html",
-            }
-        }
+if __name__ == "__main__":
 
-    for path in dic :
-        directory = Path( path )
-        for folder in dic[path] :
-            files=[ f.resolve() for f in directory.glob( dic[path][folder] ) ]
-            if files :
-                report_paths[folder]=files
+  from jawm.utils import workflow
 
-    return report_paths
+  # parse the command line jawm call
+  # if no workflow was called, workflows will default to 'main' 
+  workflows, var, args, unknown_args = jawm.utils.parse_arguments(["main","fastqc","test"])
 
+  # check which workflow was called from the command line
+  if workflow( ["main","fastqc","test"], workflows ) :
 
-from pathlib import Path
-from zipfile import ZipFile
+    # handle single file calls
+    if "f" in fastqc.var:
+      # clone the fastqc process
+      fastqc_=fastqc.clone()
+      # execute the process
+      fastqc_.execute()
 
+    # handle folder calls
+    if "input_folder" in fastqc.var :
 
-def unzip_file(zip_path, destination=None):
-    """Extract a zip file into its folder, or into destination if provided."""
+      # list all the fastq files in the input folder
+      fastq_files = [
+          os.path.join( fastqc.var["input_folder"], f )
+          for f in os.listdir( fastqc.var["input_folder"] )
+          if f.endswith( (".fastq.gz", ".fq.gz") )
+      ]
+
+      for f in fastq_files :
+        # for each fastq file clone the fastqc process
+        fastqc_=fastqc.clone()
+        # attribute the input file
+        fastqc_.var["map.f"] = f
+        # execute the process
+        fastqc_.execute()
+
+  # Wait for all processes to complete
+  jawm.Process.wait()
+
+  # when running the test workflow we do also unzip the output file
+  if workflow( ["test"], workflows ) :
+
+    from pathlib import Path
+    from zipfile import ZipFile
+
+    # unzip the output file
+    zip_path=os.path.join( fastqc.var["fastqc_output"], os.path.basename( str( fastqc.var["f"] ).lstrip().split(" ")[0].split( ".fastq.gz"  )[0].split( ".fq.gz"  )[0] )+"_fastqc.zip" )
+
     zip_path = Path(zip_path)
-    destination = zip_path.parent if destination is None else Path(destination)
+    destination = zip_path.parent
     destination.mkdir(parents=True, exist_ok=True)
 
     with ZipFile(zip_path, "r") as zip_ref:
-        for member in zip_ref.infolist():
-            target_path = destination / member.filename
-            if not target_path.resolve().is_relative_to(destination.resolve()):
-                raise ValueError(f"Unsafe zip entry: {member.filename}")
-        zip_ref.extractall(destination)
+      for member in zip_ref.infolist():
+        target_path = destination / member.filename
+        if not target_path.resolve().is_relative_to(destination.resolve()):
+          raise ValueError(f"Unsafe zip entry: {member.filename}")
+      zip_ref.extractall(destination)
 
-    return destination
+    print("Test completed")
+    sys.stdout.flush()
 
-
-if __name__ == "__main__":
-    import sys
-    from jawm.utils import workflow
-
-    workflows, var, args, unknown_args = jawm.utils.parse_arguments(["main","fastqc","test"],)
-
-    if workflow( ["main","fastqc","test"], workflows ) :
-
-        if "f" in fastqc.var.keys() :
-            if fastqc.var["f"] != "" : 
-
-                print( "Found:", fastqc.var["f"] )
-
-                # execute process
-                fastqc.execute()
-
-                # wait for all processes to complete
-                jawm.Process.wait()
-
-
-        if "fastq_folder" in fastqc.var.keys() :
-            if fastqc.var["fastq_folder"] != "" :
-
-                read_files = list(Path( fastqc.var["fastq_folder"] ).glob(f'*{fastqc.var["read1_sufix"]}'))
-
-                fastqc_jobs=[]
-
-                for f in read_files :
-
-                    fastqc_=fastqc.clone()
-                    fastqc_.var["map.f"]=f
-                    fastqc_.execute()
-                    fastqc_jobs.append(fastqc_.hash)
-
-                jawm.Process.wait( fastqc_jobs )
-
-
-    if workflow( "test", workflows ) :
-
-        zip_file=os.path.join( fastqc.var["fastqc_output"], os.path.basename( str(fastqc.var["f"]).lstrip().split(" ")[0].split( ".fastq.gz"  )[0].split( ".fq.gz"  )[0] )+"_fastqc.zip" )
-        unzip_file( zip_file )
-        print( "Test completed." )
-
-    sys.exit(0)
+sys.exit(0)
